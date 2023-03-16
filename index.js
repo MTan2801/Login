ca2828098722b3ee3ccbfd1455ae6467eab60f78
MTan <minhtan8007@gmail.com>
2023-03-16 14:48:56 +0700

    updateAll

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let apiUser ="  http://localhost:3000/user";
 
-const email = document.querySelector(".email");
-const password = document.querySelector(".password");
+const email = document.querySelector("#email");
+const password = document.querySelector("#password");
 const submit = document.querySelector(".submit");
 
 const getUser = async() =>{
@@ -9,21 +9,20 @@ const getUser = async() =>{
   const data = await response.json();
   return data;
 };
-
 submit.addEventListener("click",(e) => {
   e.preventDefault();
-  if(email ==="" || password===""){
-    alert("nhap email mk");
+  if(email.value =="" || password.value==""){
+    alert("Vui lòng nhập đủ Email & mật khẩu");
   } else{
     getUser().then((data) =>{
       const user = data.find((user) =>
-        user.email === email && user.password === password
+        user.email === email.value && user.password === password.value
       );
       if (user) {
-        alert("thanh cong");
+        alert("Đăng nhập thành công");
         window.location.href="mainPage.html";
       } else {
-        alert("that bai");
+        alert("Đăng nhập thất bại");
       }
     });
   }
@@ -37,20 +36,3 @@ function w3_open() {
   function w3_close() {
     document.getElementById("mySidebar").style.display = "none";
   }
-
-//   function ValidateEmail(inputText)
-//   {
-//   var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-//   if(inputText.value.match(mailformat))
-//   {
-//   alert("Valid email address!");
-//   document.form1.text1.focus();
-//   return true;
-//   }
-//   else
-//   {
-//   alert("You have entered an invalid email address!");
-//   document.form1.text1.focus();
-//   return false;
-//   }
-//   }
